12fae9754ba5aefdfe1248f656484565498b460a
sunil <suniltiwari460@gmail.com>
2023-08-17 13:22:27 +0530

    winston-logger

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import logger from "./config/logger.js";
 
 // dotEnv config {to access the variables}
 dotenv.config();
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 8000;
 console.log(process.env.NODE_ENV);
 
 app.listen(PORT, () => {
-  console.log(`server is listening at ${PORT}`);
+  logger.info(`server is listening at ${PORT}`);
 });
 
 console.log("kmdv");

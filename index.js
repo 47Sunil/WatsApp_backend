8f32cd4466015a12d1a5fdd8c34e1a6f8938c205
sunil <suniltiwari460@gmail.com>
2023-08-19 13:00:10 +0530

    settingUp-mongodb

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import logger from "./config/logger.js";
+import mongoose from "mongoose";
 
 // dotEnv config {to access the variables}
 dotenv.config();
 
 // env variables
+const { DATABASE_URL } = process.env;
 const PORT = process.env.PORT || 8000;
-
 console.log(process.env.NODE_ENV);
 
+// exit on mongodb error
+mongoose.connection.on("error", (err) => {
+  logger.error(`Mongodb connection error : ${err}`);
+  process.exit(1);
+});
+
+// mongodb debug mode
+if (process.env.NODE_ENV !== "production") {
+  mongoose.set("debug", true);
+}
+
+// connect mongodb
+mongoose
+  .connect(DATABASE_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Connected to mongodb");
+  });
+
+//! here we are using custom logger.
 let server = app.listen(PORT, () => {
   logger.info(`server is listening at ${PORT}`);
   //   console.log("process id", process.pid); // process has an id
   //   throw new Error("error in servere");
 });
 
-// handle server error or uncought exception and unhandled rejection error
+//! handle server error or uncought exception and unhandled rejection error.
 const exitHandler = () => {
   if (server) {
     logger.info("server closed.");
@@ -34,7 +57,7 @@ const unexpextedErrorHandler = (error) => {
 process.on("uncaughtException", unexpextedErrorHandler); // it act as an event listener.
 process.on("unhandledRejection", unexpextedErrorHandler);
 
-// SIGTERM
+//! SIGTERM
 // it is not for window, but we will need while deployment.
 process.on("SIGTERM", () => {
   if (server) {
